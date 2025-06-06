443e3d3ef8a5baad29bdf1d68d84cd4848a9c341
Benjamin Gutman <benjamingutman@icloud.com>
2025-06-06 17:17:30 -0400

    Question 3 and 4 finished

--- src/Question3/Question3.java
+++ src/Question3/Question3.java
@@ -2,7 +2,81 @@ package Question3;
 
 public class Question3{
 	
+	//There is a typo in the question description so these are the assumptions being made
+	//Lower ranking is for a lower score. if employee a has score 9, b with 5 and c with 10
+	//the order will b->a->c (0,1,2)
 	
+	
+	// as well, if there are multiple of the same number, each employee of the same number will receive
+	//the smallest value and the later employees will be accounted for like in sports
+	//eg 1, 2, 2, 4, 5, 6, 7, 7, 7, 10
+	static int[] employeeRankings(int[] scores) {
+		int N = scores.length;
+		int[] copy = new int[N];
+		int[] aux = new int[N];
+		int[] rankings = new int[N];
+		for (int i = 0; i < N; i++) {
+			copy[i] = scores[i];
+		}
+		for (int i = 1; i < N; i = i+i) {
+			int k = 0;
+			for (int j = 0 ;j<N; j += i+i) {
+				int low = j;
+				int high = 0;
+				int mid = 0;
+				if ((j + 2*i) <N) {
+					high = j+2*i;
+				}
+				else {
+					high = N;
+				}
+				if ((j + i ) < N) {
+					mid = j+i;
+				}
+				else {
+					mid = N;
+				}
+				int low2 = mid;
+				while (low < mid && low2 < high) {
+					if (copy[low] < copy[low2]) {
+						aux[k++] = copy[low++];
+					}
+					else {
+						aux[k++] = copy[low2++];
+					}
+				}
+				while (low < mid) {
+					aux[k++] = copy[low++];
+				}
+				while (low2<high) {
+					aux[k++] = copy[low2++];
+				}
+			}
+			for (int j = 0; j < N; j++) {
+				copy[j] = aux[j];
+			}
+		}
+		for (int i =0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if (scores[i] == copy[j]) {
+					rankings[i] = j;
+					break;
+				}
+			}
+		}
+		return rankings;
+	}
+	
+	
+	public static void main(String[] args) {
+		int[] scores = {5,4,9,8,6,0,2,1,4,4,6,7,4};
+		int[] results = employeeRankings(scores);
+		for(int i = 0; i <results.length; i++) {
+			System.out.println(results[i]);
+		}
+		
+		
+	}
 	
 	
 }
---
+++ src/Question4/Question4.java
@@ -0,0 +1,107 @@
+package Question4;
+
+import java.util.Random;
+
+public class Question4 {
+
+	
+	public static void main(String[] args) {
+		linkedlist list = new linkedlist();
+		for (int i =1; i <= 10; i++) {
+			list.pushFront(i);
+		}
+		linkedlist shuffledList = new linkedlist();
+		shuffledList.head = list.shuffle(list.head);
+		shuffledList.printLinkedList();
+	}
+}
+
+	
+	
+	
+class linkedlist{
+	
+	Node head;
+	
+	static class Node{
+		int data;
+		Node next;
+	}
+
+	
+	void pushFront(int Data) {
+		 Node temp = new Node();
+		 temp.data = Data;
+		 temp.next = head;
+		 head = temp;
+	}
+	
+	static Node getMid(Node Head) {
+		if(Head == null|| Head.next == null) {
+			return Head;
+		}
+		Node mid = Head;
+		Node end = Head;
+		Node beforeMid = null;
+		while (end != null && end.next != null) {
+			end = end.next.next;
+			beforeMid = mid;
+			mid = mid.next;
+		}
+		return beforeMid;
+	}
+	
+	 Node shuffle(Node Head) {
+		if (Head.next == null) {
+			return Head;
+		}
+		Node mid = getMid(Head);
+		Node second = mid.next;
+		mid.next = null;
+		
+		Node half1 = shuffle(Head);
+		Node half2 = shuffle(second);
+		
+		return merge(half1, half2);
+	}
+	
+	static Node merge(Node left, Node right) {
+		Node current = new Node();
+		Node fakeHead = current;
+		Random rand = new Random();
+		while (left != null && right != null) {
+			if (rand.nextBoolean()) {
+				current.next = left;
+				left = left.next;
+				current = current.next;
+			}
+			else {
+				current.next = right;
+				right = right.next;
+				current = current.next;
+			}
+		}
+		while (left != null) {
+				current.next = left;
+				left = left.next;
+				current = current.next;
+		}
+		while (right != null) {
+				current.next = right;
+				right = right.next;
+				current = current.next;
+		}
+		
+		return fakeHead.next;
+	}
+	
+	void printLinkedList() {
+		Node pointer = head;
+		while (pointer != null) {
+			System.out.println(pointer.data);
+			pointer = pointer.next;
+		}
+	}
+	
+}
+
